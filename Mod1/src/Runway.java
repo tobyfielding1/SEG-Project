645c5f1047f327282d6f4b35639bb4a1fc321ec6
AK10ASH <ap21g14@soton.ac.uk>
2017-03-14 22:57:28 +0000

    Fixed cleared and graded area and test cases pass

--- Mod1/src/Runway.java
+++ Mod1/src/Runway.java
@@ -175,14 +175,22 @@ public class Runway {
             return false;
         }
         
-        /*
-        //returns without redeclaration if obstacle is more than 60m from TORA and 75m from centreline
         if (this.getDirection() <= 18){
-        	if (obstacle.dist1stThresh + getThreshold() < -60 || obstacle.dist2ndThresh < -60)
-        		return false;
-        }else if (obstacle.dist2ndThresh + getThreshold() < -60 || obstacle.dist1stThresh < -60)
-        	return false;
-        */
+        	if (obstacle.dist1stThresh>getOriginalTORA()+60 || obstacle.dist1stThresh<-60 )
+            return false;
+        }
+        if (this.getDirection() > 18){
+        	if (obstacle.dist2ndThresh>getOriginalTORA()+60 || obstacle.dist2ndThresh<-60 )
+            return false;
+        }
+        
+//        //returns without redeclaration if obstacle is more than 60m from TORA and 75m from centreline
+//        if (this.getDirection() <= 18){
+//        	if (obstacle.dist1stThresh + getThreshold() < -60 || obstacle.dist2ndThresh < -60)
+//        		return false;
+//        }else if (obstacle.dist2ndThresh + getThreshold() < -60 || obstacle.dist1stThresh < -60)
+//        	return false;
+        
         calculations = "Runway: " + this.name +"\n";
         String wkngCalculations;
         int dist;
