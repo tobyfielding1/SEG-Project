4b246bb065438ae84043aedd26d9dd16a6ad0144
tobyfielding1 <tobyfielding1@gmail.com>
2017-04-18 14:50:56 +0100

    Merge branch 'master' of C:\Users\tobyf\IdeaProjects\SEG-Project with conflicts.

--- Mod1/src/Airport.java
+++ Mod1/src/Airport.java
@@ -1,12 +1,12 @@
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Set;
-
 /**
  * Created by tobyf on 18/02/2017.
  *
  * this class represents 'Model' in our MVC pattern
  */
-public class Airport {
+public class Airport implements Serializable {
 
     private final String name;
     private HashMap<String, Runway> runways = new HashMap<>();
@@ -24,14 +24,27 @@ public class Airport {
     }
 
     public void addRunway(Runway runway) {
-        runways.put(runway.name,runway);
+        runways.put(runway.getName(), runway);
     }
 
     public void removeRunway(String name) {
         runways.remove(name);
     }
 
-    public Runway getRunway(String name){
+    public Runway getRunway(String name) {
         return runways.get(name);
     }
+
+/*
+    public void save(File file) {
+// Create output stream.
+        FileOutputStream fos = new FileOutputStream(file + ".xml");
+
+        // Create XML encoder.
+        XMLEncoder xenc = new XMLEncoder(fos);
+
+        // Write object.
+        xenc.writeObject(aFoo);
+    }
+*/
 }
